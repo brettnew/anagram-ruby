67f423b4eef43511114abd35172fdc8a41f99ad7
Brett New and Lauren Posey <brett+lauren@>
2016-06-30 15:01:25 -0700

    it will display anagrams in a nice and tidy list

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/coin_combo')
+require('./lib/anagram')
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/anagram') do
+  @word_list = params.fetch("initial_word").anagram(params.fetch("word_list"))
+  @initial_word = params.fetch("initial_word")
+  erb(:anagram)
 end
--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -1,4 +1,14 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('anagram', {:type => :feature}) do
+  it('processes the user entry and returns anagrams') do
+    visit('/')
+    fill_in('initial_word', :with => 'cat')
+    fill_in('word_list', :with => 'act tac atc tca cta bat')
+    click_button('Anagram')
+    expect(page).to have_content('act tac atc tca cta')
+  end
+end
